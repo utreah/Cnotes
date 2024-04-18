4bcdb3055b9c22691c8b9de5a2a83e259b65cffa
utreah <91834152+utreah@users.noreply.github.com>
2024-04-18 17:24:34 +0300

    delete nUtils

--- 24_01_03_2024/nUtility.h
+++
@@ -1,8 +0,0 @@
-#ifndef NUTILITY_H
-#define NUTILITY_H
-// #define ISLEAP(y) ((y) % 4 == 0 && ((y) % 100 != 0 || (y) % 400 == 0))
-int isprime(int number);
-int ndigit(int number);
-void clear_input_buffer(void);
-void put_start(void);
-#endif
