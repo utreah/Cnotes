82a01eed995a92cc44cb562f021225063d70fd62
utreah <91834152+utreah@users.noreply.github.com>
2024-04-22 10:12:20 +0300

    Create 29_15_03_2024.c

---
+++ 29_15_03_2024/29_15_03_2024/29_15_03_2024.c
@@ -0,0 +1,66 @@
+#include "C:\Users\Liver\Documents\GitHub\Cnotes\nUtility.h"
+#include <iostream>
+#include <ctype.h>
+#define SIZE 100
+/*
+int main()
+{
+	char arr1[SIZE];
+	char arr2[SIZE];
+	printf("Enter two words: ");
+	scanf("%s%s", arr1, arr2);
+	printf("[%s]     [%s]\n", arr1, arr2);
+
+	int len1 = 0;
+	while (arr1[len1])
+		len1++;	
+	int len2 = 0;
+	while (arr2[len2])
+		len2++;
+
+	if (len1 != len2) {
+		printf("UZUNLUKLAR AYNI DEGIL!\n");
+		return 0;
+	}
+	int counts[26] = { 0 };
+	for (int i = 0; arr1[i] != '\0'; ++i) {
+		++counts[toupper(arr1[i]) - 'A'];
+		--counts[toupper(arr2[i]) - 'A'];
+	}
+
+	for (int i = 0; i < 26; i++)
+		if (counts[i]) {
+			printf("Degil\n");
+			return 0;
+		}
+	printf("anagram");
+}
+*/
+			/*----------------------------------------------YAZIDAN SAYIYA/SAYIDAN YAZIYA DÖNÜÞÜMLER--------------------------------------*/
+int main(void)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+------------------------------------------------------ -
+Ödev
+ANAGRAM boþluk karakterleri ve noktalama karakterleri ciddiye alýnmayacak, 
+debit card -> bad credit
+a gentlement -> elegant man
+
+
+------------------------------------------------------ -
+*/
