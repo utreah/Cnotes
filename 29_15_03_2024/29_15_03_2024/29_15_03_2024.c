fd2f794b18dda8168639ce9121aa2ca7c95945eb
utreah <91834152+utreah@users.noreply.github.com>
2024-04-22 15:18:27 +0300

    Array last and Warnings

--- 29_15_03_2024/29_15_03_2024/29_15_03_2024.c
+++ 29_15_03_2024/29_15_03_2024/29_15_03_2024.c
@@ -39,20 +39,95 @@ int main()
 			/*----------------------------------------------YAZIDAN SAYIYA/SAYIDAN YAZIYA DÖNÜÞÜMLER--------------------------------------*/
 int main(void)
 {
-
+	char str[SIZE] = { '5' };
+	printf("%d", str[0] - '0');
 }
+	
+			/*----------------------------------------------YAZIDAN SAYIYA/SAYIDAN YAZIYA DÖNÜÞÜMLER--------------------------------------*/
+// Comment Lines
+/*
+-Çeviri iþlemi olmadan önce kodtan çýkarýlýr
+-Preprocessor direktiflerinden önce kodtan çýkarýlýyor
+-Nested comment block YOK!
+-En iyi açýklama hiç yapýlmayan açýklama
+-Code -> self explanatory (kendini açýklayan) olmalý
+-Debug sürecinde bazý kodlarý comment out etmek için kullanýlabilir
+			/* #include <stdio.h> */ // -> bu ppd çalýþtýrýlmaz
+
+/*Bu bir açýklama*/
+// bu bir açýklama
+/*önce yorum sonra kod olabilri*/ int test;
+int test2; /*Once kod sonra yorum olabilir*/
+/*
+	DOSYANIN ÝSMÝ .dosya uzantisi 
+	DOSYA AÇIKLAMASI
+	YAZAR(author)	
+	last update date
+	LISANS(VAR ISE)
+	lÝCENSE:
+	*/
+					/*----------------------------------------------WARNINGs--------------------------------------*/
+/*
+-Warningleri kodu commit etmeden önce KESINLIKLE çözmeliyiz.
+
+int x; // unreferenced X 
+int y = x + 5; // uninitialized X
+
+Etkisiz kod
+
+	5;
+	a;
+	a + 5; hepsi etkisiz kod. geçerli, hata yok ama etkisiz.
+	a == 10; etkisiz. Code has no effect
+	if (x = 10) // "assignment withing the conditional statement" hatasý verir.
+
+	Aþaðýsý unreachable code
+
+	int foo(int a)
+	{
+		if(a == 1)
+			return 10;
+		else
+			return 100;
+
+		return 1000;  // hiçbir zaman ulaþamayacak
+	}
+
+	fonksiyon içersinde infinite loop durumu olabilir.
+	derleyiciler bunla ilgili mesaj verebiliyor ama vermek zorunda da deðil.
+
+	Stack Overflow hatasý
+	recursive fonksiyonlarda meydana gelir.
+
+	fonksiyon sürekli kendini çaðýrdýðýnda, otomatik ömürlü deðiþkenler sürekli
+	stackten yer harcýyor ve yeni çaðrýlan fonksiyonlarýnda otomatik ömürlü
+	deðiþkenleri stackten yer harcýyor ve stackte yer kalmýyor.
+	Buna stack overflow deniyor.
+
+	Derleyiciler bu durumu her zaman olmasada anlayabiliyorlar.
+
+	if (x)
+		return x
+	return 10    // bu duruma base case deniyor.durdurma mekanizmasý gibi.
 
 
 
+*/
+
+// inline int;
+/*
+inline anahtar sözcüðü C99 ile eklendi. 
+Fonksiyonlarý niteleyen bir anahtar sözcük denir.
+inline fonksiyonlarýn ayrý bir semantiði var.
+C ile CPPdeki inline birebir ayný deðil.
+*/
+
+/*
+int (foo)(int x) // eðer foo isimli bir fonksiyonel bir makro var ise bu durumun önüne geçmek için identifer parantez içine alýnabilir
+int foo(int x) == int (foo)(int x) -> ayný bunlar
 
 
-
-
-
-
-
-
-
+*/
 
 /*
 ------------------------------------------------------ -
@@ -61,6 +136,20 @@ ANAGRAM boþluk karakterleri ve noktalama karakterleri ciddiye alýnmayacak,
 debit card -> bad credit
 a gentlement -> elegant man
 
+------------------------------------------------------ -
+
+Ödev
+1.55
+dizi için alt sequence lar aranýyor.
+
+1234243456567956  içimde 565 sequence aranýyor ama hem dizi hem sequence random.
+------------------------------------------------------ -
+
+Ödev
+SIZE + 1 boyutundaki dizide SIZE a kadar tüm sayýlar var ama
+bir sayýdan 2 tane var. bunu bulacaðýz.
+2:00
+algoritme karmaþýklýðý nedir?
 
 ------------------------------------------------------ -
 */
