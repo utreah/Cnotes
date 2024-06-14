e4b7fe70728f6aaa60f79169c72dd18afcddd2b8
utreah <91834152+utreah@users.noreply.github.com>
2024-06-14 18:35:14 +0100

    removed unnecessary expression

--- C ÖDEVLERİ/strncpy.c
+++ C ÖDEVLERİ/strncpy.c
@@ -1,8 +1,7 @@
 typedef unsigned int size_t;
 char* my_strncpy(char* pdest, const char* psrc, size_t nwords) {
-	size_t i = 0;
 	const char* ptemp = pdest;
-	while (i++ < nwords && (*pdest++ = *psrc++))
+	while (nwords-- && (*pdest++ = *psrc++))
 		;
 	return (char*)ptemp;
 }
