b1d04906455eb3ae0028f6aea08433638fc86051
utreah <ersoyugur1528@gmail.com>
2024-11-10 18:37:47 +0000

    Date modülü, Complete - Incomplete types giriş

--- 53_29_05_2024/53_29_05_2024/53_29_05_2024.c
+++ 53_29_05_2024/53_29_05_2024/53_29_05_2024.c
@@ -12,13 +12,51 @@
 	Date adýnda bir tür oluþturucaz. Bu tür
 		13 Mayýs 1232 gibi tarihleri tutacak. 
 	Bunun için 2 adet dosya oluþturduk. Date.c ve Date.h
+int main(void) {
+	return 0;
+}
 */
-#include "Date.h"
+
+//////////////////////////////////////////////////// Buradan sonraki kýsým yeni bir konu, üstü Date.h Date.c ile alakalý kendi modülümüzü oluþturduk.
+/*											 Bir yapýnýn elemanýnýn yapý türünden olmasý 
+	Bir struct'ýn elemaný baþka bir struct türden nesne olabilir.
+	Yapýnýn elemaný, baþka bir yapý türünden pointerda olabilir
+		Bu elemanýn dereference edilebilmesi için hayatý devam eden, geçerli bir pointer nesnesi olmalý.
+
+	Bir yapý türünün elemaný kendi türünden olamaz.
+struct Eng {
+	int x, y, z;
+	// struct Eng ex; -> Bir yapý türünün elemaný kendi türünden olamaz sentaks hatasý
+};
+
+struct Ken {
+	struct Eng n;
+	struct Eng* ptr;
+};
 
 int main(void) {
-
+	struct Ken e;
+	// e.n.x;  // e. ile Ken yapýsýndaki n'ye eriþiyoruz daha sonra da Eng yapýsýndaki x elemanýna
+	e.ptr->x; // ptr bir pointer olduðu için elemanlarýna ok ile eriþiyoruz.
+	struct Ken* kenPtr;
+	//.. code
+	kenPtr->ptr->x; // Mantýk ayný. kenPtr bir pointer ve elemanlarýna -> ile eriþiyoruz. ptr nesnesi de pointer onunn da elemanlarýna -> ile eriþiyoruz
 }
+*/
 
+													/*				Complete types - Incomplete types					*/
+/*
+	Derleyici tarafýndan compile time'da complete ya da incomplete olarak niteleniyor.
 
+	Incomplete Types: Derleyici bir türün varlýðýndan haberdar fakat tanýmýný görmediyse -> Incomplete types
 
+*/
 
+struct Nec; // Derleyici bunu görüyor, fakat struct Nec'in tanýmý yok. Burada sadece bildirimi yapýlmýþ iþte buna incomplete type deniyor
+
+struct Eng; // derleyici tanýmý görüyor hem de altýnda bildirimini görüyor bu da complete type
+struct Eng {
+	int x, y;
+};
+
+// Ne iþe yaradýðýný sonraki derste.
